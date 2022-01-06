8820f95b11b19f8a26abdd0c7b2a8dcd92e6246e
Mbah Neilvani <neilvanimbah@gmail.com>
2022-01-06 09:51:33 +0100

    added another solution

--- Js/Cat_Dog years.js
+++ Js/Cat_Dog years.js
@@ -48,4 +48,24 @@ const humanYearsCatYearsDogYears = (humanYears) => {
       }
     }
     return [humanYears,catYears,dogYears];
-  }
+}
+
+//Another 
+
+var humanYearsCatYearsDogYears = function(humanYears) {
+    var catYears = 0;
+    var dogYears = 0;
+    
+    if (humanYears === 1) {
+      catYears = 15;
+      dogYears = 15;
+    } else if (humanYears === 2) {
+      catYears = 24;
+      dogYears = 24;
+    } else if (humanYears > 2) {
+      catYears = 24 + (humanYears - 2) * 4;
+      dogYears = 24 + (humanYears - 2) * 5;
+    }
+  
+    return [humanYears, catYears, dogYears];
+}
