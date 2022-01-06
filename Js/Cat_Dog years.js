99974446434604d137623d2197f6e05b5eac44f4
Mbah Neilvani <neilvanimbah@gmail.com>
2022-01-06 09:45:37 +0100

    adding ages

---
+++ Js/Cat_Dog years.js
@@ -0,0 +1,51 @@
+// I have a cat and a dog.
+
+// I got them at the same time as kitten/puppy. That was humanYears years ago.
+
+// Return their respective ages now as [humanYears,catYears,dogYears]
+
+// NOTES:
+
+// humanYears >= 1
+// humanYears are whole numbers only
+// Cat Years
+// 15 cat years for first year
+// +9 cat years for second year
+// +4 cat years for each year after that
+// Dog Years
+// 15 dog years for first year
+// +9 dog years for second year
+// +5 dog years for each year after that
+
+const years = 5;
+let catYear = 15
+let dogYear = 15
+
+function catAndDogYears(humanYears,catYears,dogYears){
+ if(let i = 5; i >= catAndDogYears.length; i++ ){
+
+ }
+}
+
+// Solution
+
+const humanYearsCatYearsDogYears = (humanYears) => {
+    let catYears = 0;
+    let dogYears = 0;
+    
+    for (let i = 1; i <= humanYears; i++) {
+      if (i === 1) {
+        catYears += 15;
+        dogYears += 15;
+      }
+      else if (i === 2) {
+        catYears += 9;
+        dogYears += 9;
+      }
+      else {
+        catYears += 4;
+        dogYears += 5;
+      }
+    }
+    return [humanYears,catYears,dogYears];
+  }
